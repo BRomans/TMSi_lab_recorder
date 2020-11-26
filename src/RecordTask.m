f02f70b68a8eb9e74d94421e7182124b3cac6806
Michele <michele.romani.gzl0@me.com>
2020-11-26 16:58:20 +0100

    :bug: fixed several bugs in the recording script, changed eeglab install location for file conversion

--- src/RecordTask.m
+++ src/RecordTask.m
@@ -5,6 +5,10 @@ function RecordTask(task_number, participant_name)
     % Step 1: Setup library and choose connection type ( 'usb', 'bluetooth', 'network' or 'wifi' ).
     library = TMSi.Library('usb');
 
+    % Subset of channels to show
+    channel_subset = [1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24]; % only show channels from 1 to 24
+
+    
     display(task_number)
     display(participant_name)
     % Step 2: Find device to connect to. Keep on trying every second.
@@ -22,17 +26,17 @@ function RecordTask(task_number, participant_name)
 
         try
             % Step 5: Set settings for sampler.
-            % sampler.setSampleRate(2048);
-            % sampler.setReferenceCalculation(true);
+            sampler.setSampleRate(2048);
+            sampler.setReferenceCalculation(true);
 
             % Step 6: Create a RealTimePlot.
-            realTimePlot = TMSi.RealTimePlot('RealTimePlot Example', sampler.sample_rate, device.channels);
-            realTimePlot.setWindowSize(10);
+            realTimePlot = TMSi.RealTimePlot('RealTimePlot Example', sampler.sample_rate, device.channels(channel_subset));
+            realTimePlot.setWindowSize(50);
             realTimePlot.show();
 
             % Step 7: Create a Poly5 file to save samples to.
-            poly5 = TMSi.Poly5('task_'+ task_number + '_' + participant_name +'.Poly5', 'Task ' + task_number + ' ' + participant_name, sampler.sample_rate, device.channels);
-
+            poly5 = TMSi.Poly5(strcat('task_',task_number, '_', participant_name,'.Poly5'), strcat('Task ',task_number,' ',participant_name), sampler.sample_rate, device.channels);
+            %poly5 = TMSi.Poly5('example04.Poly5', 'Poly5 Example', sampler.sample_rate, device.channels);
             % Step 8: Connect to device through the sampler.
             sampler.connect();
 
@@ -48,7 +52,8 @@ function RecordTask(task_number, participant_name)
                 samples = sampler.sample();
 
                 % Step 11: Append samples to plot.
-                realTimePlot.append(samples);
+                realTimePlot.append(samples(channel_subset, :));
+                % realTimePlot.append(samples);
 
                 % Step 12: Append and draw samples to poly5 file.
                 poly5.append(samples);
@@ -67,8 +72,12 @@ function RecordTask(task_number, participant_name)
 
         % Step 15: Disconnect with device.
         sampler.disconnect();
-
-        ConvertToEEGLab('task_'+ task_number + '_' + participant_name +'.Poly5')
+        try
+            %ConvertToEEGlab(strcat('task_',task_number, '_', participant_name,'.Poly5'));
+        catch matlabException
+            warning(sprintf('Error while trying to convert poly5 file for EEGLab.\n\tIdentifier: %s\n\tMessage: %s\n', matlabException.identifier, matlabException.message));
+        end
+        
         
     catch matlabException
         warning(sprintf('Error while trying to create device/sampler.\n\tIdentifier: %s\n\tMessage: %s\n', matlabException.identifier, matlabException.message));
